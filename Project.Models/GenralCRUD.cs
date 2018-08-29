64cc380b5cfe97ebf47c12386adb9d5e6d60fae4
Alok Kumar Pandey <alok_go25@hotmail.com>
2018-08-29 20:29:37 +0545

    Add files via upload

---
+++ Project.Models/GenralCRUD.cs
@@ -0,0 +1,57 @@
+﻿using SQLHelper;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Project.Models
+{
+    public class GenralCRUD
+    {
+
+        public async Task<int> AddProject(ProjectInfo objInfo)
+        {
+            List<KeyValuePair<string, object>> Param = new List<KeyValuePair<string, object>>();
+            Param.Add(new KeyValuePair<string, object>("@ProjectName", objInfo.ProjectName));
+            Param.Add(new KeyValuePair<string, object>("@ProjectCode", objInfo.ProjectCode));
+            string strSpName = "usp_AddProject";
+            SQLHandlerAsync sqlHAsy = new SQLHandlerAsync();
+            return await sqlHAsy.ExecuteNonQueryAsync(strSpName, Param);
+        }
+
+        public async Task<int> UpdateProject(ProjectInfo objInfo)
+        {
+            List<KeyValuePair<string, object>> Param = new List<KeyValuePair<string, object>>();
+            Param.Add(new KeyValuePair<string, object>("@ProjectID", objInfo.ProjectID));
+            Param.Add(new KeyValuePair<string, object>("@ProjectName", objInfo.ProjectName));
+            Param.Add(new KeyValuePair<string, object>("@ProjectCode", objInfo.ProjectCode));
+            string strSpName = "usp_UpdateProject";
+            SQLHandlerAsync sqlHAsy = new SQLHandlerAsync();
+            return await sqlHAsy.ExecuteNonQueryAsync(strSpName, Param);
+        }
+
+        public async Task<ProjectInfo> GetProject(ProjectInfo objInfo)
+        {
+            List<KeyValuePair<string, object>> Param = new List<KeyValuePair<string, object>>();
+            Param.Add(new KeyValuePair<string, object>("@ProjectID", objInfo.ProjectID));            
+            string strSpName = "usp_GetProject";
+            SQLHandlerAsync sqlHAsy = new SQLHandlerAsync();
+            //List<ProjectInfo> objList = await sqlHAsy.ExecuteAsListAsync<ProjectInfo>(strSpName, Param);
+            return await sqlHAsy.ExecuteAsObjectAsync<ProjectInfo>(strSpName, Param);
+        }
+
+        public async Task<List<ProjectInfo>> GetProjectList()
+        {
+            //List<KeyValuePair<string, object>> Param = new List<KeyValuePair<string, object>>();
+            //Param.Add(new KeyValuePair<string, object>("@ProjectID", objInfo.ProjectID));
+            //Param.Add(new KeyValuePair<string, object>("@ProjectName", objInfo.ProjectName));
+            //Param.Add(new KeyValuePair<string, object>("@ProjectCode", objInfo.ProjectCode));
+            string strSpName = "usp_GetProjectList";
+            SQLHandlerAsync sqlHAsy = new SQLHandlerAsync();
+            //List<ProjectInfo> objList = await sqlHAsy.ExecuteAsListAsync<ProjectInfo>(strSpName, Param);
+            return await sqlHAsy.ExecuteAsListAsync<ProjectInfo>(strSpName);
+        }
+    }
+
+}
---
+++ Project.Models/ProjectInfo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Project.Models
+{
+    public class ProjectInfo
+    {
+        public ProjectInfo()
+        {
+        }
+        public int ProjectID { get; set; }
+        public string ProjectName { get; set; }
+        public string ProjectCode { get; set; }
+
+    }
+}
