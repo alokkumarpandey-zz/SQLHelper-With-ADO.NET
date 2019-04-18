b963b389d9a4137e9156a3cd1c71af1bbb0212d7
Alok Kumar Pandey <alok_go25@hotmail.com>
2019-04-18 17:31:09 +0545

    Update GenralCRUD.cs

--- Project.Models/GenralCRUD.cs
+++ Project.Models/GenralCRUD.cs
@@ -1,4 +1,4 @@
-﻿using SQLHelper;
+using SQLHelper;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -9,7 +9,18 @@ namespace Project.Models
 {
     public class GenralCRUD
     {
-        public async Task<int> AddProject(ProjectInfo objInfo)
+       
+        public void AddProject(ProjectInfo objInfo)
+        {
+            List<SQLParam> Param = new List<SQLParam>();
+            Param.Add(new SQLParam("@ProjectName", objInfo.ProjectName));
+            Param.Add(new SQLParam("@ProjectCode", objInfo.ProjectCode));
+            string strSpName = "usp_AddProject";
+            SQLAddUpdate sqlHAsy = new SQLAddUpdate();
+            sqlHAsy.ExecuteNonQuery(strSpName, Param);
+        }
+
+        public async Task<int> AddProjectAsync(ProjectInfo objInfo)
         {
             List<SQLParam> Param = new List<SQLParam>();
             Param.Add(new SQLParam("@ProjectName", objInfo.ProjectName));
@@ -18,7 +29,19 @@ namespace Project.Models
             SQLAddUpdateAsync sqlHAsy = new SQLAddUpdateAsync();
             return await sqlHAsy.ExecuteNonQueryAsync(strSpName, Param);
         }
-        public async Task<int> UpdateProject(ProjectInfo objInfo)
+
+        public void UpdateProject(ProjectInfo objInfo)
+        {
+            List<SQLParam> Param = new List<SQLParam>();
+            Param.Add(new SQLParam("@ProjectID", objInfo.ProjectID));
+            Param.Add(new SQLParam("@ProjectName", objInfo.ProjectName));
+            Param.Add(new SQLParam("@ProjectCode", objInfo.ProjectCode));
+            string strSpName = "usp_UpdateProject";
+            SQLAddUpdate sqlHAsy = new SQLAddUpdate();
+            sqlHAsy.ExecuteNonQuery(strSpName, Param);
+        }
+
+        public async Task<int> UpdateProjectAsync(ProjectInfo objInfo)
         {
             List<SQLParam> Param = new List<SQLParam>();
             Param.Add(new SQLParam("@ProjectID", objInfo.ProjectID));
@@ -28,26 +51,54 @@ namespace Project.Models
             SQLAddUpdateAsync sqlHAsy = new SQLAddUpdateAsync();
             return await sqlHAsy.ExecuteNonQueryAsync(strSpName, Param);
         }
-        public async Task<ProjectInfo> GetProject(ProjectInfo objInfo)
+
+        public ProjectInfo GetProject(ProjectInfo objInfo)
         {
             List<SQLParam> Param = new List<SQLParam>();
             Param.Add(new SQLParam("@ProjectID", objInfo.ProjectID));
             string strSpName = "usp_GetProject";
-            SQLGetAsync sqlHAsy = new SQLGetAsync();
-            //List<ProjectInfo> objList = await sqlHAsy.ExecuteAsListAsync<ProjectInfo>(strSpName, Param);
+            SQLGet sqlHAsy = new SQLGet();
+            return sqlHAsy.ExecuteAsObject<ProjectInfo>(strSpName, Param);
+        }
+
+        public async Task<ProjectInfo> GetProjectAsync(ProjectInfo objInfo)
+        {
+            List<SQLParam> Param = new List<SQLParam>();
+            Param.Add(new SQLParam("@ProjectID", objInfo.ProjectID));
+            string strSpName = "usp_GetProject";
+            SQLGetAsync sqlHAsy = new SQLGetAsync();            
             return await sqlHAsy.ExecuteAsObjectAsync<ProjectInfo>(strSpName, Param);
         }
-        public async Task<IList<ProjectInfo>> GetProjectList()
+
+        public IList<ProjectInfo> GetProjectAsIList()
         {
-            //List<KeyValuePair<string, object>> Param = new List<KeyValuePair<string, object>>();
-            //Param.Add(new KeyValuePair<string, object>("@ProjectID", objInfo.ProjectID));
-            //Param.Add(new KeyValuePair<string, object>("@ProjectName", objInfo.ProjectName));
-            //Param.Add(new KeyValuePair<string, object>("@ProjectCode", objInfo.ProjectCode));
             string strSpName = "usp_GetProjectList";
-            SQLGetListAsync sqlHAsy = new SQLGetListAsync();
-            //List<ProjectInfo> objList = await sqlHAsy.ExecuteAsListAsync<ProjectInfo>(strSpName, Param);
+            SQLGetList sqlHAsy = new SQLGetList();
+            return sqlHAsy.ExecuteAsList<ProjectInfo>(strSpName);
+        }
+
+        public List<ProjectInfo> GetProjectAsList()
+        {
+            string strSpName = "usp_GetProjectList";
+            SQLGetList sqlHAsy = new SQLGetList();
+            return sqlHAsy.ExecuteAsList<ProjectInfo>(strSpName) as List<ProjectInfo>;
+        }
+
+        public async Task<IList<ProjectInfo>> GetProjectAsIListAsync()
+        {           
+            string strSpName = "usp_GetProjectList";
+            SQLGetListAsync sqlHAsy = new SQLGetListAsync();            
             return await sqlHAsy.ExecuteAsListAsync<ProjectInfo>(strSpName);
         }
+
+        public async Task<IList<ProjectInfo>> GetProjectAsListAsync()
+        {
+            string strSpName = "usp_GetProjectList";
+            SQLGetListAsync sqlHAsy = new SQLGetListAsync();
+            return await sqlHAsy.ExecuteAsListAsync<ProjectInfo>(strSpName);
+        }
+
+
     }
 
 }
